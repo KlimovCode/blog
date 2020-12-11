0e11c63058bc2ce2cad161ea138325ad21c87c0b
klimovcode <klimovcode@mail.ru>
2020-12-11 23:07:14 +0500

    layout Footer

--- client/src/common/Footer.jsx
+++ client/src/common/Footer.jsx
@@ -3,10 +3,25 @@ import { NavLink } from 'react-router-dom'
 
 export const Footer = () => {
   return (
-    <nav className="navbar fixed-bottom navbar-light bg-light">
-      <div className="container">
-        <a className="navbar-brand" href="#">Оля эндокринолог</a>
+    <div className="pt-4 my-md-5 pt-md-5 border-top">
+      <div className="row">
+        <div className="col-12 col-md">
+          Оля эндокринолог
+        </div>
+        <div className="col-6 col-md">
+          <h5>Категории</h5>
+          <ul className="list-unstyled text-small">
+            <li>
+              <a href="" className="text-muted">Диабет</a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </nav>
+    </div>
+    // <nav className="navbar fixed-bottom navbar-light bg-light">
+    //   <div className="container">
+    //     <a className="navbar-brand" href="#">Оля эндокринолог</a>
+    //   </div>
+    // </nav>
   )
 }
--- client/src/common/Nav.jsx
+++ client/src/common/Nav.jsx
@@ -14,40 +14,5 @@ export const Nav = () => {
         <NavLink to="/online" className="">Онлайн консультация</NavLink>
       </nav>
     </div>
-
-    // <div className="row">
-    //   <div className="col-12">
-    //     <header>
-    //       <nav className="navbar navbar-expand-sm navbar-light bg-light">
-    //         <NavLink className="navbar-brand" to="/">Оля эндокринолог</NavLink>
-    //         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-    //           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    //           <span className="navbar-toggler-icon"></span>
-    //         </button>
-
-    //         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    //           <ul className="navbar-nav mr-auto">
-    //             <li className="nav-item active">
-    //               <NavLink className="nav-link" to="/">Главная <span className="sr-only">(current)</span></NavLink>
-    //             </li>
-    //             <li className="nav-item">
-    //               <a className="nav-link" href="#">Обо мне</a>
-    //             </li>
-    //             <li className="nav-item">
-    //               <NavLink className="nav-link" to="/">Блог</NavLink>
-    //             </li>
-    //             <li className="nav-item">
-    //               <a className="nav-link" href="#">Контакты</a>
-    //             </li>
-    //           </ul>
-    //         </div>
-    //         <NavLink to="/online">Онлайн консультация</NavLink>
-    //       </nav>
-    //       <div>
-
-    //       </div>
-    //     </header>
-    //   </div>
-    // </div>
   )
 }
